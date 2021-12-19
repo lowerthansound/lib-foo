35d1f35dd77f145bbcdc40a1bbbf69dddbe1f3d1
lowerthansound <email@lowerthansound.pro>
2021-12-19 20:47:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version="1.0.0",
     description="foos the bar",
     author="company",
-    author_email="name@company.com",
+    author_email="johndoe@example.com",
     url="https://bitbucket.org/companyname/lib-foo",
     install_requires=[
-        "structlog",
+        "requests",
     ],
     packages=[
         "foo",
